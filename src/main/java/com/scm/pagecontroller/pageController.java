bace173decfb79703dba16e9d62d19bfc13ad086
pavan-kumar-singh <singhpavankumar25@gmail.com>
2025-01-10 23:44:20 +0530

    added some new pages and configured

--- src/main/java/com/scm/pagecontroller/pageController.java
+++ src/main/java/com/scm/pagecontroller/pageController.java
@@ -6,20 +6,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class pageController {
     @RequestMapping ("/home")
-    public String home(){
+    public String homePage(){
         System.out.println("this is home page");
         return "home";
     }
 
-    @RequestMapping ("/navbar")
-    public String nav(){
+    @RequestMapping ("/contact")
+    public String contactPage(){
         System.out.println("this is navbar page");
-        return "navbar";
+        return "contact";
     }
 
     @RequestMapping ("/about")
-    public String aboutpage(){
+    public String aboutPage(){
         System.out.println("this is about page");
         return "about";
     }
+    @RequestMapping ("/login")
+    public String loginPage(){
+        System.out.println("this is navbar page");
+        return "login";
+    }
+
+    @RequestMapping ("/register")
+    public String registerPage(){
+        System.out.println("this is navbar page");
+        return "register";
+    }
+    @RequestMapping ("/services")
+    public String servicesPage(){
+        System.out.println("this is navbar page");
+        return "services";
+    }
+
+
 }
